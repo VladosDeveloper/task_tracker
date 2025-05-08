23792ddade971fae3f8efe7bee6ad6764ede7408
VladosDeveloper <yurenya.vlad.01@mail.ru>
2025-05-08 16:08:03 +0300

    vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,11 @@ import react from "@vitejs/plugin-react-swc"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    assetsDir: "assets"
   },
   resolve: {
     alias: {
